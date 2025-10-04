d1fcd16b22d3de29c9cf0334c17bc340d2d11070
refikkarakilic <rfkkrklc95@gmail.com>
2025-10-04 16:58:00 +0300

    Q 268

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import easy.missingNumber;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+
+public class Main
+{public static void main(String[] args) {
+
+    missingNumber solver = new missingNumber();
+
+    int[] examples = {1, 2, 3, 4, 5, 6, 8};
+    int result = solver.missingNumber(examples);
+    System.out.println(result);
+
+}
 }
